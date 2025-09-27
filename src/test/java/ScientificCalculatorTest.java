72d9b3759e1a385e023fa31ab22217c9d121f638
Anurag <Anurag.Ramaswamy@iiitb.ac.in>
2025-09-27 22:25:28 +0530

    Testing using JUnit and Build using Maven

---
+++ src/test/java/ScientificCalculatorTest.java
@@ -0,0 +1,36 @@
+package src.test.java;
+import org.junit.jupiter.api.Test;
+import src.main.java.ScientificCalculator;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ScientificCalculatorTest
+{
+    @Test
+    public void testSqrt()
+    {
+        assertEquals(3.0,ScientificCalculator.sqrt(9),0.0001);
+        assertThrows(IllegalArgumentException.class,() -> ScientificCalculator.sqrt(-1));
+    }
+
+    @Test
+    public void testFactorial()
+    {
+        assertEquals(720,ScientificCalculator.factorial(6));
+        assertEquals(1,ScientificCalculator.factorial(0));
+        assertThrows(IllegalArgumentException.class,() -> ScientificCalculator.factorial(-3));
+    }
+
+    @Test
+    public void test_natural_log()
+    {
+        assertEquals(1.0,ScientificCalculator.natural_log(Math.E),0.0001);
+        assertThrows(IllegalArgumentException.class,() -> ScientificCalculator.natural_log(0));
+    }
+
+    @Test
+    public void testPower()
+    {
+        assertEquals(343.0,ScientificCalculator.power(7,3),0.0001);
+        assertEquals(1.0,ScientificCalculator.power(7,0),0.0001);
+    }
+}
